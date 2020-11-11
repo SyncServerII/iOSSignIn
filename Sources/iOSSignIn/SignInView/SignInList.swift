c744a2ac555129340b40e578599a7cdb6cfbc28d
Christopher Prince <chris@SpasticMuffin.biz>
2020-11-10 22:04:18 -0700

    Attempting to deal with list updating issue.

--- Sources/iOSSignIn/SignInView/SignInList.swift
+++ Sources/iOSSignIn/SignInView/SignInList.swift
@@ -4,8 +4,13 @@ struct SignInList: View {
     @ObservedObject var model:SignInModel
     
     var body: some View {
-        List(model.currentSignIns) { signIn in
-            SignInRow(description: signIn)
+        List {
+            // Embedding `ForEach` to attempt to deal with updating problem.
+            // The list wasn't updating when the `model.currentSignIns` changed
+            // or when the list items went off screen.
+            ForEach(model.currentSignIns) { signIn in
+                SignInRow(description: signIn)
+            }
         }
     }
 }
